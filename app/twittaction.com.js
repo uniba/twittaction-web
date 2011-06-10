4d7f4d30eaf811b618509916c728bbb99d4b856f
daichi <prinum101@gmail.com>
2011-06-10 20:34:22 +0900

    postしたデータの保存。getでデータを取得

--- app/twittaction.com.js
+++ app/twittaction.com.js
@@ -29,34 +29,13 @@ app.post('/action', function(req, res) {
     var userId='';
     var message='';
     var sequence='';
+    var key = '';
     userId = req.body.userId;
     message =  req.body.message;
     sequence =  req.body.sequence;
-    
-
- 
-    // http://b.ruyaka.com/2011/03/24/node-jsmongoose%E3%81%A7mongodb%E3%82%92%E8%A9%A6%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B-serversmanvps/
     var Action = mongoose.model('Action');
-    var action = new Action();
-    action.userId = userId;
-    action.message = message;
-    action.sequence.push(sequence);
-    action.created;
     
-    action.save(function(err) {
-        if(err){ 
-            res.send(err);
-        }else{
-            //res.send("送信成功しました。");
-            
-            //var _id = ;
-            var short = "http://twt.ac/" + _id
-            
-            
-            res.send(short);
-            //res.send({ userId: userId , message:message , sequence:sequence});
-        }
-    });//action.save(function(err) {
+    save(userId,message,sequence,Action,res);//自作関数
     
 });//app.post('/action', function(req, res) {
 
@@ -64,4 +43,65 @@ app.post('/action', function(req, res) {
 if (!module.parent) {
 	app.listen(3000);
 	console.log("Express server listening on port %d", app.address().port);
+}
+
+/*
+app.post('/action', function(req, res) {});
+	の中で使用する関数。
+    引数は次のようにする。
+    userId = req.body.userId;
+    message =  req.body.message;
+    sequence =  req.body.sequence;
+    var Action = mongoose.model('Action');
+    res = res        function(req, res)のres 
+    この関数は,
+    
+    action.save(function(err) {
+        if(err){         
+            save(userId,message,sequence,Action,res);
+        }else{
+            //res.send("送信成功しました。");
+            res.send(short);
+        }
+    
+    この部分で使う。
+*/
+function save(userId,message,sequence,Action,res){
+    var userId = userId;
+    var message = message;
+    var sequence = sequence;
+    var key = '';
+   
+    var random = new Array(0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','m','n','l','o','p','q','r','s','t','u','v','w','x','y','z');
+    
+    for (var i = 0 ; i <=6  ; i++){
+        arrayKey = (Math.floor(Math.random () * 36));
+        key += random[arrayKey];
+    }
+    
+     /*
+    var random = new Array(0,1,2);
+    
+    for (var i = 0 ; i <=1  ; i++){
+        arrayKey = (Math.floor(Math.random () * 3));
+        key += random[arrayKey];
+    }//３つの数字の中から、2桁の数字のkeyの組み合わせは 3*3=9 通り。keyが被ったら別のkeyを作成するかのテスト。 すでに、9個のkeyがDBにあったら、無限ループに入る。
+    */
+    var short = "http://twt.ac/action/:" + key;
+    var Action = Action;
+    
+    var action = new Action();
+    action.userId = userId;
+    action.message = message;
+    action.sequence.push(sequence);
+    action.key = key;
+    
+    action.save(function(err) {
+        if(err){         
+            save(userId,message,sequence,Action,res);
+        }else{
+            //res.send("送信成功しました。");
+            res.send(short);
+        }
+    });
 }
--- app/twt.ac.js
+++ app/twt.ac.js
@@ -2,13 +2,54 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var app = module.exports = express.createServer();
+var express = require('express'),
+	app = module.exports = express.createServer();
+	mongoose = require('mongoose'),
+	schema = require(__dirname + '/../lib/schema')
+;
+
+app.configure(function() {
+	schema.define(mongoose, function(mongoose) {
+		mongoose.connect(process.env.MONGODB_URI);
+	});
+});
 
 // Routes
 app.get('/', function(req, res) {
 	res.send({ server_name: "twt.ac" });
 });
+
+
+app.get('/action/:key', function(req, res) {
+        
+    /* プログラムでは、「/action/:key」　のように、「:」　が書いてあるが、
+     * 入力する、url　では、「:」を入力する必要がない！
+     * そのまま取得できる。
+     */
+
+	// http://expressjs.com/guide.html#routing の Route Param Pre-conditions
+    // http://hideyukisaito.com/doc/expressjs/guide/ ルート・パラメータの事前処理
+    // http://blog.starbug1.com/archives/854 少し参考になる
+    
+    
+    var key=req.params.key;
+    
+    /* http://b.ruyaka.com/2011/03/24/node-jsmongoose%E3%81%A7mongodb%E3%82%92%E8%A9%A6%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B-serversmanvps/
+     *下の方のコード
+    */
+    // http://d.hatena.ne.jp/KrdLab/20110317/1300367785
+    // http://mongoosejs.com/docs/api.html model の finds document
+    var Action = mongoose.model('Action');
+    Action.find({key:key}, function(err, docs) {
+       /*res.send(docs);
+       * これをやると、取得したデータが一覧表示される。jsonみたい。
+       *
+       */
+       res.send(docs[0].sequence);
+    });
+    
+});//app.get('/action/:key', function(req, res ,next) {
+
 
 // Only listen on $ node app.js
 if (!module.parent) {
