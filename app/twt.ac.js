1c3499ce59132444475f932f89b851577b72b83c
daichi <prinum101@gmail.com>
2011-07-07 13:48:08 +0900

    エラーが起こっても、サーバーが落ちないようにした。エラーが起こったときのエラー画面はまだうまく出来ない

--- app/twt.ac.js
+++ app/twt.ac.js
@@ -11,6 +11,7 @@ var express = require('express'),
 app.configure(function() {
 	schema.define(mongoose, function(mongoose) {
 		mongoose.connect(process.env.MONGODB_URI);
+        
 	});
 });
 
@@ -39,22 +40,31 @@ app.get('/action/:key', function(req, res) {
     // http://d.hatena.ne.jp/KrdLab/20110317/1300367785
     // http://mongoosejs.com/docs/api.html model の finds document
     var Action = mongoose.model('Action');
+    
+    
+    try{
+    
     Action.find({key:key}, function(err, docs) {
        // res.send(docs);
        /*これをやると、取得したデータが一覧表示される。jsonみたい。
        *
        */
-       
-    if(!err){   
-       
+    
+    // エラー処理 なんかうまくいかない。エラーが起こっても、落ちなくなったが、send するのは1回だけ
+   process.on('uncaughtException', function (err) {
+             res.send('URLが間違っているようです');
+          console.log('uncaughtException => ' + err);
+
+
+    });
+
+
+
+
        //以下のhtml ファイルは改行すると SyntaxError になる。
        res.send('<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0" /><title>Twittaction</title><link rel="stylesheet" href="http://twittaction.com/movement/css/basic.css"/><script src="http://twittaction.com/movement/js/jquery.1.6.js"></script><script src="http://twittaction.com/movement/js/jsdeferred.js"></script><script src="http://twittaction.com/movement/js/move.js"></script><script src="http://twittaction.com/movement/js/twittaction.js"></script></head><body>  <header>    <h1>Twittaction</h1>  </header>  <div id="wrapper">    <div id="stage">      <div class="inner">        <div id="cube">          <div class="inner" id="cube-inner">            <div class="side-a"></div>            <div class="side-b"></div>            <div class="side-c"></div>            <div class="side-d"></div>            <div class="side-e"></div>            <div class="side-f"></div>          </div>        </div>      </div>    </div>  </div>  <script>var tmp ='       +docs[0].sequence[0]+       '; var data = [];    for (var i = 0, len = tmp["X"].length; i < len; i++) {        data.push({            x: tmp["X"][i] * 5,            y: tmp["Y"][i] * 5,            z: tmp["Z"][i] * 5        });    };    Twittaction("cube-inner").add(data).execute();  </script></body></html>');
     });
     
-    }else{
-        res.send('aaa');
-    
-    }
     
 });//app.get('/action/:key', function(req, res ,next) {
 
